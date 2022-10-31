98c252fd9ef9874c445cc7f8bb7bd4ab504050ca
Kenar <93253836+bloodwarriorr@users.noreply.github.com>
2022-10-31 11:10:30 +0200

    Update Constants.tsx

--- src/utils/Constants.tsx
+++ src/utils/Constants.tsx
@@ -1,21 +1,18 @@
-//קבועים לציון גודל חלון
+//window size
 export const CLIENT_HEIGHT = window.innerHeight;
 export const CLIENT_WIDTH = window.innerWidth;
 export const TILE_SIZE = CLIENT_WIDTH / 13;
-//קבוע לציון אורך הALERT
+//alert length
 export const TOAST_DURATION = 3000;
-//קבוע אשר מבצע חישוב,בכל פעימה של MILI SEC 
-//ישמר תאריך משחק חדש-משמש כאשר ממזערים את המשחק וחוזרים אליו
 export const PLAY_DATE_TIMER = 60000;
-//קבוע אשר בכל פעימה יעלה את כמות החיים באחד
 export const REMAINING_GAMES_TIMER = 180000;
-//הסתברויות לחישוב קושי השלב-רנדומלי או חכם עפ"י דרגת קושי שלב
+//level difficulty consts
 export const PROBABILITY_DIFFICULTY_EASY = 2;
 export const PROBABILITY_DIFFICULTY_MEDIUM = 5;
 export const PROBABILITY_DIFFICULTY_HARD = 7;
-//מהירות המשחק
+//game speed
 export const GAME_SPEED = 4;
-//קבוע לשליטה בסיבוב דמויות
+//css const
 export const DIR_CSS: { [key: string]: string } = {
   RIGHT: "scaleX(1) translate(10%,-25%)",
   LEFT: "scaleX(-1) translate(15%,-25%)",
